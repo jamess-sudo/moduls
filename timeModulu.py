a07f2f7e07f1dc9ec5041ba8d4437a1131d611ba
jamess-sudo <aj369369aj@gmail.com>
2026-03-24 13:25:13 +0400

    Add files via upload

---
+++ timeModulu.py
@@ -0,0 +1,25 @@
+import time as t
+
+
+# baslangic=t.time()
+# list1=[]
+
+# for i in range(10000000):
+#     list1.append(i)
+
+# bitis=t.time()
+# print(bitis-baslangic)
+
+
+
+
+# zaman = t.ctime()
+# print(zaman)
+
+
+# zaman = t.strftime("%d:%m:%Y %H:%M:%S")
+# print(zaman)
+
+# print("salam")
+# zaman=t.sleep(5)
+# print("sagol")
