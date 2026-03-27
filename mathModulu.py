8ff1030a8bb61c12514bff6a7bcd9686ea119969
jamess-sudo <aj369369aj@gmail.com>
2026-03-27 11:58:59 +0400

    Add files via upload

---
+++ mathModulu.py
@@ -0,0 +1,45 @@
+# import math as m
+
+# a=m.factorial(5)
+
+# print(a)
+
+
+
+
+import random
+
+# for i in range(10):
+#     print(random.random())
+
+# for i in range(10):
+#     print(random.uniform(1,10))
+
+# for i in range (10):
+#     print(random.randint(1,10))
+
+# for i in range (10):
+#     print(random.randrange(1,10,2))
+
+
+
+
+# list1=["Goy", "Qirmizi", "Yasil", "Ag", "Benovseyi"]
+# for i in range (10):
+#     print(random.choice(list1))
+
+
+# list1=["Goy", "Qirmizi", "Yasil", "Ag", "Benovseyi"]
+# for i in range(10):
+#     random.shuffle(list1)
+#     print(list1)
+
+
+# list1=["Goy", "Qirmizi", "Yasil", "Ag", "Benovseyi"]
+# for i in range(10):
+#     print(random.sample(list1,2))
+
+
+
+
+
